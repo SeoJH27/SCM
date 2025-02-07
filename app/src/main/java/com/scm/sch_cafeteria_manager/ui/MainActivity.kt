9d417e8afbebf1421ba3ec22d9068ae2a16ddd71
SeoJH27 <morninglibrary@gmail.com>
2025-02-07 21:59:54 +0900

    FEAT: 기본 네비게이션 세팅

---
+++ app/src/main/java/com/scm/sch_cafeteria_manager/ui/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.scm.sch_cafeteria_manager.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import com.scm.sch_cafeteria_manager.R
+import com.scm.sch_cafeteria_manager.databinding.ActivityHomeBinding
+
+class MainActivity: AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding= ActivityHomeBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        setNavigation()
+    }
+
+    private fun setNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.container_home) as NavHostFragment
+
+        val navController = navHostFragment.navController
+
+    }
+
+}
