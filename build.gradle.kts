045dd7e8e1ba6d85aa51d8be93446cf0ecdba54f
SeoJH <morninglibrary@gmail.com>
2026-06-12 14:00:41 +0900

    FIX: gitignore 규칙 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
-    dependencies {
-        classpath(libs.androidx.navigation.safe.args.gradle.plugin)
-    }
-}
-
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.androidx.navigation.safeargs) apply false
     id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
 }
